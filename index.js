6e59c36b73b1821ff0bfd57a9bb640dae6cbda81
Juma <gerald.juma@student.moringaschool.com>
2025-03-11 13:42:35 +0300

    continued

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+let saturdayFun = (activity = "roller-skate") => {
+    return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = (activity = "go to the office") => {
+    return `This Monday, I will ${activity}.`
+}
+
+let wrapAdjective = (flair = "*") => {
+    return (adjective = "a hard worker") => {
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
